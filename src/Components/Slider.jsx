ad11872aba38a38b4002de1a10b0b13ea45f4474
samuel Mwangi <samuelmwangi729@gmail.com>
2024-04-14 11:35:26 -0700

    created a Slider caption

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -8,19 +8,89 @@ class Slider extends Component {
       <div className='slider'>
         <OwlCarousel className="owl-theme" lazyLoad={true} autoplay={true} autoplayTimeout={5000} animateIn={'flipInX'} animateOut={'fadeOut'} smartSpeed={450} loop margin={10} items={1} >
             <div class='item slider1'>
-                <h4>1</h4>
+                <div className="slider-content">
+                    <h2 className='heading'>
+                        Unraveling the Mysteries
+                    </h2>
+                    <div className="explanation">
+                        <p>
+                        Delve into the depths of academia, where every discovery leads to a multitude of
+                        questions. Embark on a journey of knowledge.
+                        </p>
+                    </div>
+                    <div className="cta">
+                        <button className='btn btn-default'>Begin Exploration</button>
+                        <button className='btn btn-primary'>Learn More</button>
+                    </div>
+                </div>
             </div>
             <div class='item slider2'>
-                <h4>2</h4>
+                <div className="slider-content">
+                    <h2 className='heading'>
+                        Innovative Research at Work
+                    </h2>
+                    <div className="explanation">
+                        <p>
+                            Witness the transformative power of research, where curiosity 
+                            meets creativity, and ideas shape the future of scholarship.
+                        </p>
+                    </div>
+                    <div className="cta">
+                        <button className='btn btn-primary'>Explore Research</button>
+                        <button className='btn btn-default'>Discover More</button>
+                    </div>
+                </div>
             </div>
             <div class='item slider3'>
-                <h4>3</h4>
+                <div className="slider-content">
+                    <h2 className='heading'>
+                        Empowering Minds, Shaping Futures
+                    </h2>
+                    <div className="explanation">
+                        <p>
+                        Experience the dynamic exchange of ideas, where educators
+                        inspire and students thrive, forging paths to success.
+                        </p>
+                    </div>
+                    <div className="cta">
+                        <button className='btn btn-primary'>Explore Programs</button>
+                        <button className='btn btn-default'>Register</button>
+                    </div>
+                </div>
             </div>
             <div class='item slider4'>
-                <h4>4</h4>
+                <div className="slider-content">
+                    <h2 className='heading'>
+                        Global Perspectives, Local Impact
+                    </h2>
+                    <div className="explanation">
+                        <p>
+                            Explore the interconnectedness of knowledge, where diverse perspectives
+                            converge to address local and global challenges.
+                        </p>
+                    </div>
+                    <div className="cta">
+                        <button className='btn btn-primary'>Discover Impac</button>
+                        <button className='btn btn-default'>Engage Globally</button>
+                    </div>
+                </div>
             </div>
             <div class='item slider5'>
-                <h4>5</h4>
+                <div className="slider-content">
+                    <h2 className='heading'>
+                        Innovative Learning Environments
+                    </h2>
+                    <div className="explanation">
+                        <p>
+                        Step into the future of education, where technology and tradition merge
+                        to create immersive and engaging learning experiences.
+                        </p>
+                    </div>
+                    <div className="cta">
+                        <button className='btn btn-primary'>Experience</button>
+                        <button className='btn btn-default'>Explore</button>
+                    </div>
+                </div>
             </div>
         </OwlCarousel>
       </div>
