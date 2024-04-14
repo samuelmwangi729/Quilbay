b6cb4fa40f1bbba4235f103bbc1b06956880732a
samuel Mwangi <samuelmwangi729@gmail.com>
2024-04-14 07:37:47 -0700

    Navigation Bar complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { FaBarsStaggered } from "react-icons/fa6";
 import { PiCaretDownLight } from "react-icons/pi";
+import { FaTimes } from "react-icons/fa";
 class Header extends Component {
     constructor(){
         super();
+        this.openMenu = this.openMenu.bind(this);
+    }
+    state = {
+      menuOpen: false
+    }
+    openMenu = ()=>{
+      console.log(this.state.menuOpen)
+      this.setState({
+        menuOpen:!this.state.menuOpen
+      })
     }
   render = ()=> {
     return (
@@ -18,66 +29,134 @@ class Header extends Component {
                 </h1>
               </a>
             </div>
-            {/* create the navigation links  */}
-            <div className="navbar-menus">
-              <ul className="navbar still-mobile">
-                <li className="nav-item">
-                  <a className="nav-link" href="/">About Us</a>
-                </li>
-                <li className="nav-item has-submenu service">
-                  <a className="nav-link" href="/contact">Services</a> 
-                  <div className="icon is-submenu">
-                    <PiCaretDownLight/>
+            {this.state.menuOpen ? (
+              <div className="navbar-menus">
+                <ul className="navbar still-mobile">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">About Us</a>
+                  </li>
+                  <li className="nav-item has-submenu service">
+                    <a className="nav-link" href="/contact">Services</a> 
+                    <div className="icon is-submenu">
+                      <PiCaretDownLight/>
+                    </div>
+                      <ul className="submenu">
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Course Work</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Content Writing</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Article Writing</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Copy Writing</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Editing & Proofreading</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Ghost Writing</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Technical Writing</a>
+                        </li>
+                        <li className="submenu-item">
+                          <a href="#" className="menu-link">Academic Writing</a>
+                        </li>
+                      </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/about">Portfolio</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">Blog</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/register">Contact Us</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/register">FAQs</a>
+                  </li>
+                </ul>
+                <div className="auth-buttons">
+                  <button className="btn btn-primary">Register</button>
+                  <button className="btn btn-default">Login</button>
+                </div>
+              </div>
+            ) : (
+              <div className="large-menus">
+                <div className="navbar-menus">
+                  <ul className="navbar still-mobile">
+                    <li className="nav-item">
+                      <a className="nav-link" href="/">About Us</a>
+                    </li>
+                    <li className="nav-item has-submenu service">
+                      <a className="nav-link" href="/contact">Services</a> 
+                      <div className="icon is-submenu">
+                        <PiCaretDownLight/>
+                      </div>
+                        <ul className="submenu">
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Course Work</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Content Writing</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Article Writing</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Copy Writing</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Editing & Proofreading</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Ghost Writing</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Technical Writing</a>
+                          </li>
+                          <li className="submenu-item">
+                            <a href="#" className="menu-link">Academic Writing</a>
+                          </li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/about">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/login">Blog</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/register">Contact Us</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/register">FAQs</a>
+                    </li>
+                  </ul>
+                  <div className="auth-buttons">
+                    <button className="btn btn-primary">Register</button>
+                    <button className="btn btn-default">Login</button>
                   </div>
-                    <ul className="submenu">
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Course Work</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Content Writing</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Article Writing</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Copy Writing</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Editing & Proofreading</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Ghost Writing</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Technical Writing</a>
-                      </li>
-                      <li className="submenu-item">
-                        <a href="#" className="menu-link">Academic Writing</a>
-                      </li>
-                    </ul>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">Portfolio</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/login">Blog</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/register">Contact Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/register">FAQs</a>
-                </li>
-              </ul>
-              <div className="auth-buttons">
-                <button className="btn btn-primary">Register</button>
-                <button className="btn btn-default">Login</button>
+                </div>
               </div>
-            </div>
+            )}
+            {/* create the navigation links  */}
+            
             <div className="mobile-menu-bar">
-              <div className="icon-menu menu-mobile">
-                <FaBarsStaggered/>
-              </div>
+              {this.state.menuOpen? (
+                <div className="icon-menu menu-mobile" onClick={this.openMenu}>
+                  <FaTimes/>
+                </div>
+              ) : (
+                <div className="icon-menu menu-mobile" onClick={this.openMenu}>
+                  <FaBarsStaggered/>
+                </div>
+              )}
+              
             </div>
           </nav>
         </div>
