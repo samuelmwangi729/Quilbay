abd88de87392fb48f8e8b24f74829ab5ae4e4e42
samuel Mwangi <samuelmwangi729@gmail.com>
2024-04-14 04:41:34 -0700

    Created a responsive Navigation Bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { FaBarsStaggered } from "react-icons/fa6";
+import { PiCaretDownLight } from "react-icons/pi";
 class Header extends Component {
     constructor(){
         super();
@@ -23,8 +24,37 @@ class Header extends Component {
                 <li className="nav-item">
                   <a className="nav-link" href="/">About Us</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/contact">Services</a>
+                <li className="nav-item has-submenu service">
+                  <a className="nav-link" href="/contact">Services</a> 
+                  <div className="icon is-submenu">
+                    <PiCaretDownLight/>
+                  </div>
+                    <ul className="submenu">
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Course Work</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Content Writing</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Article Writing</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Copy Writing</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Editing & Proofreading</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Ghost Writing</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Technical Writing</a>
+                      </li>
+                      <li className="submenu-item">
+                        <a href="#" className="menu-link">Academic Writing</a>
+                      </li>
+                    </ul>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/about">Portfolio</a>
@@ -42,6 +72,11 @@ class Header extends Component {
               <div className="auth-buttons">
                 <button className="btn btn-primary">Register</button>
                 <button className="btn btn-default">Login</button>
+              </div>
+            </div>
+            <div className="mobile-menu-bar">
+              <div className="icon-menu">
+                <FaBarsStaggered/>
               </div>
             </div>
           </nav>
